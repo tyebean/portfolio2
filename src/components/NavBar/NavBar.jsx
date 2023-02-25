b0bc1b27da0f61de154c9837a356b3ec265947d0
tyebean <flower.webster@gmail.com>
2023-02-24 22:31:40 -0500

    add nav

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,13 @@
+const NavBar = () => {
+  return (
+    <>
+    <div>
+      <img src="" alt="" />
+      <a href="/"><p>Home</p></a>
+      <a href="/contact"><p>Contact Me</p></a>
+    </div>
+    </>
+  );
+}
+
+export default NavBar;
