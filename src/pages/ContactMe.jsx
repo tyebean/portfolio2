2072f01a55056c0a7f662a60c72a9c4501da568e
tyebean <flower.webster@gmail.com>
2023-02-25 00:06:37 -0500

    add a contact me page

---
+++ src/pages/ContactMe.jsx
@@ -0,0 +1,11 @@
+import OneContact from '../components/OneContact/OneContact.jsx'
+
+const ContactMe = () => {
+  return (
+    <>
+    {<OneContact />}
+    </>
+  );
+}
+
+export default ContactMe;
