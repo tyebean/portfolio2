f7f04bef1b9c8f1224b5aa0cd6d554bf24533781
tyebean <flower.webster@gmail.com>
2023-02-25 00:06:27 -0500

    add one contact

---
+++ src/components/OneContact/OneContact.jsx
@@ -0,0 +1,15 @@
+const ContactMe = () => {
+  return (
+    <>
+    <p>There will be a box here with a perfect little perview of my social links!
+      <div>
+        <img src="" alt="" />
+        <h1>Title</h1>
+        <p>A little description</p>
+      </div>
+    </p>
+    </>
+  );
+}
+
+export default ContactMe;
