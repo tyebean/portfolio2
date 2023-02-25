0bd18be0ee943b17c0129645bb9f8a76c259908e
tyebean <flower.webster@gmail.com>
2023-02-25 01:56:13 -0500

    import css & add placeholder

---
+++ src/components/Projects/Projects.jsx
@@ -0,0 +1,22 @@
+import './Projects.css'
+
+const Projects = () => {
+  return (
+    <>
+      <div className='projects-div'>
+
+        <h1>Projects</h1>
+
+        <p>proj plaseholder</p>
+        <p>proj plaseholder</p>
+        <p>proj plaseholder</p>
+        <p>proj plaseholder</p>
+        <p>proj plaseholder</p>
+        <p>proj plaseholder</p>
+
+      </div>
+    </>
+  );
+}
+
+export default Projects;
