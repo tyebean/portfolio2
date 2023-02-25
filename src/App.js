91246eb2e3d7d0836213e3ccf940cba8f5718fbb
tyebean <flower.webster@gmail.com>
2023-02-24 23:33:40 -0500

    fix getting components

--- src/App.js
+++ src/App.js
@@ -3,14 +3,19 @@ import './App.css';
 import { Routes, Route } from "react-router-dom";
 import TellMeAbtYrslf from './components/TellMeAbtYrslf/TellMeAbtYrslf.jsx';
 import ContactMe from './components/ContactMe.jsx';
+import Skills from './components/Skills.jsx'
 
 function App() {
   return (
     <>
       <div className="App">
+        {<TellMeAbtYrslf />}
+        {<ContactMe />}
+        {<Skills />}
+
         <Routes>
-          <Route path="/" element={<TellMeAbtYrslf />} />
-          <Route path="/" element={<ContactMe />} />
+          <Route path="/" element={}/>
+          {/* <Route path="/contact" element={<ContactMe />} /> */}
         </Routes>
       </div>
     </>
